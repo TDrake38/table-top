553d0eb3f03ad7fe14e32b9766a84e7016a988be
TDrake38 <thomasdrake_38@hotmail.com>
2022-07-28 20:15:54 -0230

    Got the home page component to load. But navbar will not import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+
 
 function App() {
   return (
-    <div>
-      Welcome to the start of the Table Top Reporter website!!!! 
-    </div>
+    <>
+      <Container>
+        <Home/>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import { NavBar } from 'react-bootstrap'
+
+// //This is not importing for some reason
+// function NavigationBar () {
+//     return (
+//         <>
+//             <NavBar bg="primary" variant="dark">
+
+//             </NavBar>
+//         </>
+//     )
+// }
+
+// export default NavigationBar;
