f49a6d4884aace6a096e3e96ff6e4f0aba01f224
TDrake38 <thomasdrake_38@hotmail.com>
2022-10-26 22:12:54 -0600

    added and image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Home.module.css";
+import twitBackground from "../images/twitter-background.jpg"
 
 
 function Home () {
@@ -8,7 +9,13 @@ function Home () {
       <div className={styles.homie}>
         <h1 className={styles.title} >The Table Top Reporter!</h1>
         <div className={styles.newsPaper}>
-          <p>Hello</p>
+          <div id="blurp" className={styles.blurb}>
+            <h3 className={styles.h3}>Welcome to all things tabletop!</h3>
+            <p>Hello</p>
+          </div>
+          <div>
+            <img src={twitBackground} alt="twitter background" className={styles.twitBKG}/>
+          </div>
         </div>
       </div>
     </>
