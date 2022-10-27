066108b215478d02d56db0b07bf7b45375fd8e0b
TDrake38 <thomasdrake_38@hotmail.com>
2022-10-26 22:32:32 -0600

    made a little blurb and started on implementing the spotify api

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,10 +11,13 @@ function Home () {
         <div className={styles.newsPaper}>
           <div id="blurp" className={styles.blurb}>
             <h3 className={styles.h3}>Welcome to all things tabletop!</h3>
-            <p>Hello</p>
+            <p className={styles.para}>My passion for DnD has driven me to begin this website. Here you can keep up to date with the latest tweets and spotify episodes. You will also be able to find all of my social media links here in order to contact me with ideas and feedback. Enjoy!</p>
           </div>
           <div>
             <img src={twitBackground} alt="twitter background" className={styles.twitBKG}/>
+          </div>
+          <div className={styles.spotify}>
+            <p>hello</p>
           </div>
         </div>
       </div>
