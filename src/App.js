fed32d4a84930a598e95b9965b03710447c96847
TDrake38 <thomasdrake_38@hotmail.com>
2022-10-26 21:27:21 -0600

    started working on components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from "react";
 import { Container } from "react-bootstrap";
 //import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import styles from "../src/components/App.module.css";
 
 
 function App() {
   return (
-    <>
-      <Container>
-        <Home/>
-      </Container>
+    <> 
+      <div className={styles.background}>
+        <Container>
+          <Home/>
+        </Container>
+      </div>
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 function Home () {
   return (
     <>
       <div>
-        <h1>Welcome to the start of the Table Top Reporters website!!!!</h1>
+        <h1 className={styles.homie} >Welcome to the Table Top Reporter!!!!</h1>
       </div>
     </>
   );
