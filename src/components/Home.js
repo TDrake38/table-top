9a2719b77d69b5b74724dd77f227bf9a2e0fd281
TDrake38 <thomasdrake_38@hotmail.com>
2022-10-27 21:20:42 -0600

    layout close to being responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./Home.module.css";
 import twitBackground from "../images/twitter-background.jpg"
+import spotify32 from "../images/spotify-32.png"
+import twitter32 from "../images/twitter-32.png"
+import fb32 from "../images/facebook-3-32.png"
 
 
 function Home () {
@@ -8,7 +11,7 @@ function Home () {
   window.onSpotifyIframeApiReady = (IFrameAPI) => {
     let element = document.getElementById('embed-iframe');
     let options = {
-      width: '45%',
+      width: '90%',
       height: '300',
         uri: 'spotify:episode:2fI66ALYulJfKV56tEN86E'
       };
@@ -19,7 +22,12 @@ function Home () {
   return (
     <>
       <div className={styles.homie}>
-        <h1 className={styles.title} >The Table Top Reporter!</h1>
+        <div className={styles.header}>
+        <a href="https://open.spotify.com/show/4ggk2LZstPiXlDI870CMXG?si=8459761b71ea4dac"><img src={spotify32}/></a>
+        <a href="https://twitter.com/TTR110983?ref_src=twsrc%5Etfw%7Ctwcamp%5Eembeddedtimeline%7Ctwterm%5Escreen-name%3ATTR110983%7Ctwcon%5Es2"><img src={twitter32}/></a>
+        <a href="https://www.facebook.com/TTReporter"><img src={fb32}/></a>
+          <h1 className={styles.title} >The Table Top Reporter!</h1>
+        </div>
         <div className={styles.newsPaper}>
           <div id="blurp" className={styles.blurb}>
             <h3 className={styles.h3}>Welcome to all things tabletop!</h3>
@@ -28,9 +36,15 @@ function Home () {
           <div>
             <img src={twitBackground} alt="twitter background" className={styles.twitBKG}/>
           </div>
-          <div id="embed-iframe">
+          <div id="embed-iframe" className={styles.spott}>
+          </div>
+          <div className={styles.secondBlurb}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div> 
-          <a class="twitter-timeline" data-width="250" data-height="500" data-theme="dark" href="https://twitter.com/TTR110983?ref_src=twsrc%5Etfw">Tweets by TTR110983</a>
+          <a class="twitter-timeline" className={styles.tweet} data-width="800" data-height="500" data-theme="dark" href="https://twitter.com/TTR110983?ref_src=twsrc%5Etfw">Tweets by TTR110983</a>
+          <h4 className={styles.h4}>Thank you for visiting!</h4>
         </div>
       </div>
     </>
